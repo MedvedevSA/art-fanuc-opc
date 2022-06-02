56589de51220859e3a45824d3b9a2d08d0cc7b82
Medvedev Stas <medvedevsa97@gmail.com>
2022-06-02 03:41:56 +0300

    Функция control_emission, первая версия

--- Controller/DataStoreController.py
+++ Controller/DataStoreController.py
@@ -37,28 +37,76 @@ class DataStoreController():
 
         return response
 
+    def control_emissions(self, cur_status):
+        """
+            Если статус работы станка изменился меньше чем на минуту убирает запись о изменении состояния
+        """
+
+        #Из последних двух записей получить объекты datetime
+        last, prev = list(
+            map(
+                lambda string : datetime.strptime(string, '%Y-%m-%d %H:%M:%S.%f'),
+                [row.log_time for row in self.last_status_log]
+            )
+        )
+        
+
+        if (last - prev).seconds < 60:
+            self.db_session.delete(
+                self.last_status_log[1]
+            )
+
+
+    def is_status_changed(self, cur_status):
+        """
+        Проверочная функция
+
+        return True - Если последний статус отличается от текущего
+
+        return False - Если изменений нету
+        """
+
+        if self.last_status_log == None:
+            return True
+
+        if cur_status['run'] == self.last_status_log[0].client_status:
+            return False
+        else:
+            return True
+
+
+    def get_last_status_log(self, client_ip):
+        #Получить последние 2 записи client_ip из БД
+        return (
+            self.db_session.query(StatusLog)
+            .order_by(StatusLog.log_time.desc())
+            .filter(StatusLog.client_ip == client_ip)
+            .limit(2)
+        )   
+
+
+
     def save_status_to_db(self, data):
 
         for client_ip, log_time, status in data:
+            
+            self.last_status_log = self.get_last_status_log(client_ip)
 
-            last_saved_log = (
-                self.db_session.query(StatusLog)
-                .order_by(StatusLog.id.desc())
-                .filter(StatusLog.client_ip == client_ip)
-                .first()
-            )   
+            if self.is_status_changed(status):
 
-            if last_saved_log != None:
-                if status['run'] == last_saved_log.client_status:
-                    continue
-
-            self.db_session.add(
-                StatusLog(
-                    client_ip=client_ip,
-                    log_time=log_time,
-                    client_status=int(status['run'])
+                self.control_emissions(status)
+            
+                self.db_session.add(
+                    StatusLog(
+                        client_ip=client_ip,
+                        log_time=log_time,
+                        client_status=int(status['run'])
+                    )
                 )
-            )
+
+            else:
+                continue
+
 
 
         self.db_session.commit()
